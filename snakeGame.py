029b19cfcff58df5b194acc6a6188b6069a749f7
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2021-11-18 03:02:29 -0500

    change game font from monaco to arial
    change game over message and score to be in arial font

--- snakeGame.py
+++ snakeGame.py
@@ -45,7 +45,7 @@ initscore = 0
 
 # Game Over function
 def gameOver():
-    myFont = pygame.font.SysFont('monaco', 72) #choose font name and size
+    myFont = pygame.font.SysFont('arial', 72) #choose font name and size
     GOsurf = myFont.render(' YOU LOST -- GAME OVER !!!', True, red) # this is the surface where game over will display having 3 args : the message, antialiasing,and Color
     GOrect = GOsurf.get_rect() #to get rect coordinates of the game over text surface
     GOrect.midtop = (360, 15)
@@ -57,7 +57,7 @@ def gameOver():
     sys.exit() # exit cmd console
 
 def showScore(choice=1):
-    sFont = pygame.font.SysFont('monaco', 42) #choose font name and size
+    sFont = pygame.font.SysFont('arial', 42) #choose font name and size
     Ssurf = sFont.render('SCORE : {0}'.format(score), True, white) # this is the surface where game over will display having 3 args : the message, antialiasing,and Color
     Srect = Ssurf.get_rect() #to get rect coordinates of the game over text surface
     if choice == 1:
